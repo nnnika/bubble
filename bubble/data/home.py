ed35264da10f1808c79b7c9d261d8d0e9b654682
Xiaoshi Wu <nika@Xiaoshiteki-MBP.lan>
2022-02-13 22:48:37 +0800

    x

--- bubble/data/home.py
+++ bubble/data/home.py
@@ -2,15 +2,15 @@ import time
 
 # 今日最佳股票
 stock_pool = {
-    "best_stock":[{
-        "name":"比亚迪", "code":"002594.SZ", "strategy":"樱盘策略1", "strategy_profit":0.05
+    "best_stock": [{
+        "name": "比亚迪", "code": "002594.SZ", "strategy": "樱盘策略1", "strategy_profit": 0.05
     }]
 }
 
 # 未来一周大盘走势预测 (过去30天+未来5天), x轴为日期，y轴为收盘价
 index_forecast = {
-    "forecast": {"date":[], "close":[]},     # 5days
-    "real":{"date":[], "close":[]}           # 30days
+    "forecast": {"date": [], "close": []},     # 5days
+    "real": {"date": [], "close": []}           # 30days
 }
 
 
@@ -21,7 +21,7 @@ def get_index_quote(code, start, end, fields):
 
 # 未来一周上涨行业预测
 industry_forecast = {
-    "info":[{
-        "industry":"新能源", "name":"比亚迪", "code":"002594.SZ", "forecast_proft":0.05
+    "info": [{
+        "industry": "新能源", "name": "比亚迪", "code": "002594.SZ", "forecast_profit": 0.05
     }]
 }
