1834512c93ee77d996b8140bc47cb907520be74b
Wally <1161512420@qq.com>
2022-07-17 11:49:52 +0800

    add auth bp

--- bubble/api/user.py
+++ bubble/api/user.py
@@ -4,50 +4,15 @@ import os
 from hashlib import md5
 from flask import request
 from flask import Blueprint
-from flask_login import LoginManager, login_user
 from bubble.utils.logger import log
 from bubble.utils.func import pack_res, save_file
 from bubble.data.user import User
 from bubble.utils.gen_jwt import user_id_to_token, token_to_user_id
 from bubble.utils.const import (RESP_LOGIN_EXPIRED, RESP_SUCCESS)
 from jwt.exceptions import ExpiredSignatureError
-from bubble.api.base import db,login_manager
+from bubble.api.base import db
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return str(user_id)
-
-
-@user_bp.route('/login', methods=['POST'])
-def login():
-    param_dic = {}
-    if request.method == "GET":
-        param_dic = request.args
-    if request.method == "POST":
-        if request.content_type is None:
-            pass
-        elif request.content_type.startswith('application/json'):
-            param_dic = request.json
-        elif request.content_type.startswith('multipart/form-data'):
-            param_dic = request.form
-        else:
-            param_dic = request.values
-    username = param_dic.get('username')
-    password = param_dic.get('password')
-    user = User.query.filter_by(username=username, password=md5(password.encode('utf8')).hexdigest()).first()
-    # if user and user.check_password(request.form.password.data):
-    if not user:
-        return pack_res({}, code=-1, msg="用户不存在")
-    login_user(user)
-
-    print(user.id)
-    token = user_id_to_token(user.id)
-    return pack_res({
-        "token": token
-    }, code=200, msg="success")
 
 
 @user_bp.route('/info', methods=['POST', 'GET'])
@@ -77,31 +42,6 @@ def info():
         "name": "wally",
         "avatar": avatar_url,
         "introduction": "a test account"
-    }
-    return pack_res(info, code=200, msg="success")
-
-
-@user_bp.route('/logout', methods=['POST'])
-def logout():
-    param_dic = {}
-    if request.method == "GET":
-        param_dic = request.args
-    if request.method == "POST":
-        if request.content_type is None:
-            pass
-        elif request.content_type.startswith('application/json'):
-            param_dic = request.json
-        elif request.content_type.startswith('multipart/form-data'):
-            param_dic = request.form
-        else:
-            param_dic = request.values
-    # token = param_dic.get('token')
-    #     user = User.query.filter_by(username=username, password=md5(password.encode('utf8')).hexdigest()).first()
-    #     # if user and user.check_password(request.form.password.data):
-    #     if not user:
-    #         return pack_res({}, code=-1, msg="用户不存在")
-    info = {
-
     }
     return pack_res(info, code=200, msg="success")
 
